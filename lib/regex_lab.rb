5daca706cc18585bc0b0567dfa94ba9e315a4eee
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-05 16:41:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,7 +25,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan (/\d{10}\s[()-]/)
+  if phone.match (/\d{10}\s[()-]/)
     return true
   else
     return false
