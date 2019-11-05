3eb56ea69259a85b203aa60b4cebc23ca141d06b
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-05 16:40:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,7 +25,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.grep (/\d{10}\s[()-]/)
+  if phone.scan (/\d{10}\s[()-]/)
     return true
   else
     return false
