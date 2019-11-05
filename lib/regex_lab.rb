ac983b1c5f1cd4f2b33223bacab24abb77c27295
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-05 15:04:25 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,11 +2,13 @@ require 'pry'
 def starts_with_a_vowel?(word)
   if word.match (/\w+[aeiou]/)
     return true
+  else
+    return false
   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  
+
 end
 
 def words_five_letters_long(text)
